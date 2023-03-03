08b2574d840234211f0348e3ca39ba7ea582d163
mattfaucher <mattfaucher@github.com>
2023-03-03 21:46:34 +0000

    Add basic CLI

---
+++ cli.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+if __name__ == '__main__':
+    # Define the command line arguments
+    parser = argparse.ArgumentParser(description='Perform a raw HTTP GET')
+    parser.add_argument('url', metavar='URL', type=str, help='The URL to make a GET request to.')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Call the main method with the specified URL
+    main(args.url)
