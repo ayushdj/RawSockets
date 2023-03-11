b54abe5c42a77b3f119f7b5ad7870fb830b38018
mattfaucher <mattfaucher@github.com>
2023-03-11 22:47:23 +0000

    added remaining network impl, need to break up TLS handshake into 3 methods

--- RawSocket.py
+++ RawSocket.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 import time
 import urllib.parse
 
@@ -31,7 +32,6 @@ class MyRawSocket:
         self.receiving_socket = socket.socket(
             socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP
         )
-        self.syn_start_time = None
 
     def determine_url(self, url):
         """
@@ -52,66 +52,73 @@ class MyRawSocket:
         tcp_header_length = ((segment[ip_header_length + 12] >> 4) & 0xF) * 4
         return segment[ip_header_length : ip_header_length + tcp_header_length]
 
-    def send_syn_packet(self, dest_ip, dest_port):
-        """
-        todo
-        """
-        pass
-
-    def send_acknowledgement(self, source_ip, dest_ip, source_port, tcp_head):
+    def send_ack(self, source_ip, dest_ip, source_port, tcp_header):
         """
         Send ACK message.
 
         Args:
             source_ip: source IP address
             dest_ip: destination IP address
-            source_port: source port
-            tcp_head: tcp header to read
+            source_port (int): source port
+            tcp_header (bytes): tcp headers
         """
         ip_header = make_ip_header(54322, source_ip, dest_ip)
-        tcp_sequence_num = tcp_head[3]
-        tcp_ack_sequence_num = tcp[2] + 1  # must increment ack seq number
-        tcp_fin = 0
-        tcp_syn = 0
-        tcp_rst = 0
-        tcp_psh = 0
-        # ACK is only 1 value in headers
-        tcp_ack = 1
+        tcp_sequence_num = tcp_header[3]
+        tcp_ack_sequence_num = tcp_header[2] + 1  # must increment ack seq number
+
         tcp_header = make_tcp_header(
-            source_port,
-            tcp_sequence_num,
-            tcp_ack_sequence_num,
-            tcp_fin,
-            tcp_syn,
-            tcp_rst,
-            tcp_psh,
-            tcp_ack,
+            source_port, tcp_sequence_num, tcp_ack_sequence_num, 0, 0, 0, 0, 1
         )
         tcp_header = make_tcp_header(
             source_port,
             tcp_sequence_num,
             tcp_ack_sequence_num,
-            tcp_fin,
-            tcp_syn,
-            tcp_rst,
-            tcp_psh,
-            tcp_ack,
+            0,
+            0,
+            0,
+            0,
+            1,
             tcp_header,
             source_ip,
             dest_ip,
             "",
         )
-        packet = ip_header + tcp_header
-        self.sending_socket.sendto(packet, (dest_ip, 0))
+        self.sending_socket.sendto(ip_header + tcp_header, (dest_ip, 0))
 
-    def receive_acknowledgement(self):
+    # TODO refactor into multiple functions
+    def receive_synack(self, source_ip, dest_ip, src_port):
         """
-        todo
+        Receive a SYNACK from the server.
         """
+        tcp_header = None
+        while 1:
+            packet = self.receiving_socket.recvfrom(BUFFER_LENGTH)[0]
+            ip_header = struct.unpack("!BBHHHBBH4s4s", packet[:20])
+            version = (ip_header[0] >> 4) & 0xF
+            ip_header_len = version * 4
+            ttl = ip_header[5]
+            source_ip_addr = socket.inet_ntoa(ip_header[8])
+            dest_ip_addr = socket.inet_ntoa(ip_header[9])
+            tcp_header = packet[ip_header_len : ip_header_len + 20]
+            tcp_header = struct.unpack("!HHLLBBHHH", tcp_header)
 
-    def make_syn(self, source_ip, dest_ip, source_port) -> None:
+            if (
+                source_ip_addr == dest_ip
+                and dest_ip_addr == source_ip
+                and ttl == 18
+                and src_port == tcp_header[1]
+                and self.syn_start_time - time.time() < 60
+            ):
+                self.send_ack(source_ip, dest_ip, src_port, tcp_header)
+                break
+            else:
+                self.send_syn(source_ip, dest_ip, src_port)
+                break
+        return tcp_header
+
+    def send_syn(self, source_ip, dest_ip, source_port) -> None:
         """
-        Create a syn message.
+        Create and send syn message.
 
         Args:
             source_ip: source IP address
@@ -121,14 +128,23 @@ class MyRawSocket:
         ip_header = make_ip_header(54321, source_ip, dest_ip)
         # Make initial TCP header
         tcp_header = make_tcp_header(source_port, 0, 0, 0, 1, 0, 0, 0)
-        # Recreate TCP header with checksum included.
+        # Recreate TCP header with checksum included
         tcp_header = make_tcp_header(
-            tcp_header, source_port, 0, 0, 0, 1, 0, 0, 0, source_ip, dest_ip, ""
+            source_port,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            tcp_header=tcp_header,
+            source_ip=source_ip,
+            dest_ip=dest_ip,
+            data="",
         )
 
-        # final full packet - syn packets dont have any data
-        packet = ip_header + tcp_header
         # Send the packet
-        self.sending_socket.sendto(packet, (dest_ip, 0))
+        self.sending_socket.sendto(ip_header + tcp_header, (dest_ip, 0))
         # Record the time at which SYN is starting
-        self.start_time = time.time()
+        self.syn_start_time = time.time()
